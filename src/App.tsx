22c535439bc9fa8a78f16d233398ba05757e7b1c
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-12-21 14:17:29 -0500

    content

--- src/App.tsx
+++ src/App.tsx
@@ -16,43 +16,55 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Buffalo December 2024</h1>
+    <div id="container">
+      <header>
+        <h1>Buffalo December 2024</h1>
+        <img id="bills-img" />
+        <img id="jets-img" />
+      </header>
 
-      <img id="bills-img" />
-      <img id="jets-img" />
-
-      <h2>Hotel</h2>
-      <div>
-        <strong>Check-in</strong> Thu, Dec 26, 3:00PM. <strong>Check-out</strong> Mon, Dec 30, noon
+      <div className="section">
+        <h2>Hotel</h2>
+        <div>
+          <strong>Check-in:</strong> Thu, Dec 26, 3:00PM
+        </div>
+        <div>
+          <strong>Check-out</strong> Mon, Dec 30, noon
+        </div>
+        <div>
+          <a href="https://www.google.com/maps/place/Country+Inn+%26+Suites+by+Radisson,+Buffalo+South+I-90,+NY/@42.8324863,-78.7933444,17z/data=!4m9!3m8!1s0x89d30ef6c2d864bf:0x96efffd99cd01e27!5m2!4m1!1i2!8m2!3d42.8324824!4d-78.7907695!16s%2Fg%2F11g__2b4v?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
+            Country Inn & Suites by Radisson, Buffalo South I-90, NY Expedia
+          </a>
+        </div>
+        <div>Itinerary: 72989913686821</div>
+        <div>164b Slade Ave, Buffalo, NY, 14224 United States of America</div>
+        <div>Suite, 1 Bedroom</div>
+        <div>Travel Guard Policy Confirmation: 981258467</div>
       </div>
-      <div>
-        <a href="https://www.google.com/maps/place/Country+Inn+%26+Suites+by+Radisson,+Buffalo+South+I-90,+NY/@42.8324863,-78.7933444,17z/data=!4m9!3m8!1s0x89d30ef6c2d864bf:0x96efffd99cd01e27!5m2!4m1!1i2!8m2!3d42.8324824!4d-78.7907695!16s%2Fg%2F11g__2b4v?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
-          Country Inn & Suites by Radisson, Buffalo South I-90, NY Expedia
-        </a>
+
+      <div className="section">
+        <h2>Game</h2>
+        <div>Insurance? TODO, I have it, look it up</div>
+        <div>Ticket Protector II EUSP2461187567 $132.00</div>
+        <div>Buffalo Bills vs. New York Jets at Highmark Stadium Dec 29, 1:00PM</div>
       </div>
-      <div>Itinerary: 72989913686821</div>
-      <div>164b Slade Ave, Buffalo, NY, 14224 United States of America</div>
-      <div>Suite, 1 Bedroom</div>
-      <div>Travel Guard Policy Confirmation: 981258467</div>
 
-      <h2>Game</h2>
-      <div>Insurance? TODO, I have it, look it up</div>
-      <div>Ticket Protector II EUSP2461187567 $132.00</div>
-      <div>Buffalo Bills vs. New York Jets at Highmark Stadium Dec 29, 1:00PM</div>
+      <div className="section">
+        <h2>Stadium Parking</h2>
+        <div>Confirmation number 3000-0625-3346-3694-8</div>
+        <div>General Admission ticket in Section LOT7</div>
+      </div>
 
-      <h2>Stadium Parking</h2>
-      <div>Confirmation number 3000-0625-3346-3694-8</div>
-      <div>General Admission ticket in Section LOT7</div>
-
-      <h2>TODO</h2>
-      <ul>
-        <li>Snow pants</li>
-        <li>Kissing Bridge</li>
-        <li>Make a poster</li>
-        <li>Toronto?</li>
-        <li>Basho mp3</li>
-      </ul>
+      <div className="section">
+        <h2>TODO</h2>
+        <ul>
+          <li>Snow pants</li>
+          <li>Kissing Bridge</li>
+          <li>Make a poster</li>
+          <li>Toronto?</li>
+          <li>Basho mp3</li>
+        </ul>
+      </div>
     </div>
   );
 };
