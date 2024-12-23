dfa8960258999b6daae11d9bc9301dac7effc2a2
Jonathan Brink <jonathandavidbrink@gmail.com>
2024-12-23 10:56:49 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -83,27 +83,35 @@ const App = () => {
 
         <div className="section">
           <h2>Hotel</h2>
-          <div>Suite, 1 Bedroom</div>
+          <div>
+            164b Slade Ave, West Seneca, NY 14224{" "}
+            <a href="https://www.google.com/maps/place/164b+Slade+Ave,+West+Seneca,+NY+14224/">
+              Map
+            </a>
+          </div>
           <div>
             <strong>Check-in:</strong> Thu, Dec 26, 3:00PM
           </div>
           <div>
             <strong>Check-out:</strong> Mon, Dec 30, noon
           </div>
-          <div>
-            <a href="https://www.google.com/maps/place/Country+Inn+%26+Suites+by+Radisson,+Buffalo+South+I-90,+NY/@42.8324863,-78.7933444,17z/data=!4m9!3m8!1s0x89d30ef6c2d864bf:0x96efffd99cd01e27!5m2!4m1!1i2!8m2!3d42.8324824!4d-78.7907695!16s%2Fg%2F11g__2b4v?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
-              Country Inn & Suites by Radisson, Buffalo South I-90, NY Expedia
-            </a>
-          </div>
-          <div>164b Slade Ave, Buffalo, NY, 14224 USA</div>
-          <div>Itinerary: 72989913686821</div>
+          <div>Suite, 1 Bedroom, Booked through Expedia, Itinerary: 72989913686821</div>
           <div>Travel Guard Policy Confirmation: 981258467</div>
         </div>
 
         <div className="section">
           <h2>Game Dec 29, 1:00PM</h2>
+          <div>
+            <strong>Section: 216, Row 5, Seats 1-3</strong>
+          </div>
           <div>Insurance? TODO, I have it, look it up</div>
           <div>Ticket Protector II EUSP2461187567 $132.00</div>
+          <div>Tickets are in Apple Wallet</div>
+          <div>
+            <a href="https://www.google.com/maps/dir/164b+Slade+Ave,+West+Seneca,+NY+14224/Highmark+Stadium,+Bills+Drive,+Orchard+Park,+NY/@42.8056686,-78.8244353,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89d30ef5b97d497f:0xca83aab437c3c149!2m2!1d-78.7910484!2d42.833521!1m5!1m1!1s0x89d305815466a99f:0xf0b562b249eaa7f5!2m2!1d-78.7869723!2d42.7737546!3e2?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
+              Walking directions
+            </a>
+          </div>
         </div>
 
         <div className="section">
@@ -112,17 +120,42 @@ const App = () => {
           <div>
             General Admission ticket in <strong>Section LOT7</strong>
           </div>
+          <div>Ticket is in Apple Wallet</div>
+        </div>
+
+        <div className="section">
+          <h2>Kissing Bridge</h2>
+          <div>
+            <a href="https://www.kissingbridge.com/">link</a>{" "}
+            <a href="https://www.google.com/maps/place/10296+State+Rd,+Glenwood,+NY+14069">map</a>{" "}
+            <a href="https://www.google.com/maps/dir/10296+State+Rd,+Glenwood,+NY+14069/164b+Slade+Ave,+West+Seneca,+NY+14224/@42.723179,-78.8874793,11z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89d3aaabac20d5ff:0xa5b4b26b585dc217!2m2!1d-78.653319!2d42.6125819!1m5!1m1!1s0x89d30ef5b97d497f:0xca83aab437c3c149!2m2!1d-78.7910484!2d42.833521!3e0?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">
+              directions
+            </a>
+          </div>
+          <div>Mon-Wed: 10am-9pm, Thurs-Fri 10am-10pm, Sat 9am-10pm, Sun 9am-8pm</div>
+          <div>
+            Kissing Bridge is located at 10296 State Road Glenwood NY 14069. <strong>note</strong>{" "}
+            that many online maps or GPS resources will try to send you to KB via Murray Hill Rd.
+            This is a seasonal road and is impassable during the winter months.
+          </div>
+          <div>Tickets can be purchased online or at the resort.</div>
+          <div>Yes, we offer ski and snowboard rentals, including boots and helmets.</div>
+          <div>
+            Yes, we have a variety of dining options on-site, ranging from quick snacks to hot
+            meals.
+          </div>
         </div>
 
         <div className="section">
           <h2>TODO</h2>
           <ul>
             <li>Snow pants</li>
-            <li>Kissing Bridge</li>
             <li>Make a poster</li>
-            <li>Toronto?</li>
             <li>Basho mp3</li>
-            <li>Move tickets to TicketMaster Wallet so you don't need to rely on the network</li>
+            <li>Basho mp3</li>
+            <li>Basho mp3</li>
+            <li>Basho mp3</li>
+            <li>Basho mp3</li>
           </ul>
         </div>
       </div>
